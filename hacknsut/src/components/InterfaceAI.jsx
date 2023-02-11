b4d9d1fc8d0c6c1d5edf16562f7dd2b8688b513a
Parth Mahajan <parthmahajan08092003@gmail.com>
2023-02-11 21:04:38 +0530

    final ai-validator

--- hacknsut/src/components/InterfaceAI.jsx
+++ hacknsut/src/components/InterfaceAI.jsx
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 const { Configuration, OpenAIApi } = require("openai");
 // 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
 
- function InterfaceAI() {
-
-  useState
-
+function InterfaceAI() {
   const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: "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",
   });
-
-  async function fun(e) {
+  const [prompt, setPrompt] = useState("");
+  async function sendAi(e) {
     e.preventDefault();
-    console.log("hi");
+    // console.log("hi");
     const openai = new OpenAIApi(configuration);
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: "Validate the following idea : a platform for businesses to connect, collaborate, and validate their idea, support innovation",
+      prompt: prompt,
       max_tokens: 100,
       temperature: 0,
     });
-    console.log(response.data.choices[0].text);
+    const doc = document.querySelector(".resultAI");
+    // console.log(response.data.choices[0].text);
+    doc.innerHTML = response.data.choices[0].text;
+    doc.style.backgroundColor = "rgba(255, 255, 255,0.2)";
+    setPrompt("empty");
   }
-
+  let baseprompt = "Validate the Idea : ";
   return (
     <div className="interfaceAI">
       <div className="resultAI"></div>
@@ -31,8 +32,9 @@ const { Configuration, OpenAIApi } = require("openai");
           type="text"
           className="inputTextAI"
           placeholder="Write a prompt for your mail:"
+          onChange={(e) => setPrompt(baseprompt + e.target.value)}
         />
-        <button className="btn buttonAI" onClick={fun}>
+        <button type="submit" className="btn buttonAI" onClick={sendAi}>
           <i className="fa fa-paper-plane"></i>
         </button>
       </form>
--- hacknsut/src/components/TextAI.jsx
+++ hacknsut/src/components/TextAI.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
 function TextAI() {
   return (
     <div className="textAI">
-        <p className='textOneAI'>Do You Have An Idea ?</p>
-        <p className='textTwoAI'>
-            Let's validate it with AI, trained with 10000s
-            <br />
-            of data
-        </p>
+      <p className="textOneAI">Do You Have An Idea ?</p>
+      <p className="textTwoAI">
+        Let's validate it with AI, trained with 10000s
+        <br />
+        of data
+      </p>
     </div>
   );
 }
