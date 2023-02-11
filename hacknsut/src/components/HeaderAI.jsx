25ea9cef10da7b78adbdca360e50805ab8f6a1cd
Bhavesh-Gihar <97470501+Bhavesh-Gihar@users.noreply.github.com>
2023-02-11 18:55:48 +0530

    Create HeaderAI.jsx

---
+++ hacknsut/src/components/HeaderAI.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function HeaderAI() {
+  return (
+    <div className="header"><h4>How it works ? <i className="fa fa-user-circle" aria-hidden="true"></i></h4></div>
+  );
+}
+
+export default HeaderAI;
