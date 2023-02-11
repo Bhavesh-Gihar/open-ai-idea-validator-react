432b91ae1cce486e51dda224999ff878c396d86d
Bhavesh-Gihar <97470501+Bhavesh-Gihar@users.noreply.github.com>
2023-02-11 20:12:50 +0530

    Update InterfaceAI.jsx

--- hacknsut/src/components/InterfaceAI.jsx
+++ hacknsut/src/components/InterfaceAI.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from 'react';
 const { Configuration, OpenAIApi } = require("openai");
 // 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
 
+ function InterfaceAI() {
 
-function InterfaceAI() {
-    const sendAI = () => {
-      const configuration = new Configuration({
-        apiKey: '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',
-      });
-      const openai = new OpenAIApi(configuration);
-      const response = openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Say this is a test",
-        max_tokens: 7,
-        temperature: 0,
-      });
-    }
+  useState
+
+  const configuration = new Configuration({
+    apiKey: "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",
+  });
+
+  async function fun(e) {
+    e.preventDefault();
+    console.log("hi");
+    const openai = new OpenAIApi(configuration);
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Validate the following idea : a platform for businesses to connect, collaborate, and validate their idea, support innovation",
+      max_tokens: 100,
+      temperature: 0,
+    });
+    console.log(response.data.choices[0].text);
+  }
+
   return (
     <div className="interfaceAI">
       <div className="resultAI"></div>
@@ -25,7 +32,7 @@ function InterfaceAI() {
           className="inputTextAI"
           placeholder="Write a prompt for your mail:"
         />
-        <button className="btn buttonAI" onClick={sendAI}>
+        <button className="btn buttonAI" onClick={fun}>
           <i className="fa fa-paper-plane"></i>
         </button>
       </form>
